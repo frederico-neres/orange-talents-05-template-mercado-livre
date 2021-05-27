e11ebd4df00a87f9093927c46f584ccc980d04c6
Frederico Neres <neresfrederico@gmail.com>
2021-05-27 14:16:57 -0300

    add class controller file

---
+++ src/main/java/br/com/zupacademy/frederico/treinomercadolivre/treinomercadolivre/api/detalheController.java
@@ -0,0 +1,9 @@
+package br.com.zupacademy.frederico.treinomercadolivre.treinomercadolivre.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/detalhes")
+public class detalheController {
+}
